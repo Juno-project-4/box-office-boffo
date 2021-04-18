462dc6e84937fad81f206587cbdcc49cfdf2821b
christiesunnie <christiesunnie@gmail.com>
2021-04-18 00:05:20 -0400

    Added data to Firebase

--- src/App.js
+++ src/App.js
@@ -50,102 +50,113 @@ function App() {
     //         });
     // }, []);
 
-    useEffect(() => {
-        const searchUrl = "https://api.themoviedb.org/3/search/movie";
-        const apiKey = "a95c3731bb8d542ff3503355315d717a";
+    // const [movies, setMovies] = useState([]);
 
-        // API call function
-        const movieSearch = (year, page) => {
-            const url = new URL(searchUrl);
-            url.search = new URLSearchParams({
-                api_key: apiKey,
-                query: "joy",
-                primary_release_year: year,
-                page: page,
-            });
-            return url;
-        };
-        // Call the API to get the data with default page 1
-        fetch(movieSearch(2020, 1))
-            .then((res) => {
-                return res.json();
-            })
-            .then((jsonResponse) => {
-                // Create an empty array to contain the data array from all of the pages
-                const arrayOfAllPages = [];
-                // Get the total pages from the first API to loop each page of data
-                for (let i = 1; i <= jsonResponse.total_pages; i++) {
-                    fetch(movieSearch(2020, i))
-                        .then((res) => {
-                            return res.json();
-                        })
-                        .then((jsonResponse) => {
-                            // Push the data array of each page into arrayOfAllPages
-                            arrayOfAllPages.push(jsonResponse.results);
-                            // Test if the array have all of the pages after the loop
-                            if (
-                                arrayOfAllPages.length ===
-                                jsonResponse.total_pages
-                            ) {
-                                // Passing the year to create the condition and test if the release date of the movie in the range of summer period
-                                const selectYear = (year) => {
-                                    arrayOfAllPages.map((array) => {
-                                        // filter the array to get only the movies in the summer period of that year
-                                        array.filter((dataObj) => {
-                                            const now = new Date();
-                                            const releasedDate = new Date(
-                                                dataObj.release_date
-                                            );
-                                            const newReleasedDate1 = new Date(
-                                                `${year}-05-01`
-                                            );
-                                            const newReleasedDate2 = new Date(
-                                                `${year}-09-04`
-                                            );
-                                            if (
-                                                releasedDate >=
-                                                    newReleasedDate1 &&
-                                                releasedDate <= newReleasedDate2
-                                            ) {
-                                                console.log(
-                                                    `Title: ${dataObj.title}`,
-                                                    `- ${
-                                                        releasedDate < now
-                                                            ? "Released"
-                                                            : "Release"
-                                                    } date: ${
-                                                        dataObj.release_date
-                                                    }`
-                                                );
-                                            }
-                                        });
-                                    });
-                                };
-                                selectYear(2020);
-                            }
-                        });
-                }
-            });
-    }, []);
+    // useEffect(() => {
+    //     const searchUrl = "https://api.themoviedb.org/3/search/movie";
+    //     const apiKey = "a95c3731bb8d542ff3503355315d717a";
+
+    //     // API call function
+    //     const movieSearch = (year, page) => {
+    //         const url = new URL(searchUrl);
+    //         url.search = new URLSearchParams({
+    //             api_key: apiKey,
+    //             query: "love",
+    //             primary_release_year: year,
+    //             page: page,
+    //         });
+    //         return url;
+    //     };
+    //     // Call the API to get the data with default page 1
+    //     fetch(movieSearch(2000, 1))
+    //         .then((res) => {
+    //             return res.json();
+    //         })
+    //         .then((jsonResponse) => {
+    //             // Create an empty array to contain the data array from all of the pages
+    //             const arrayOfAllPages = [];
+    //             // Get the total pages from the first API to loop each page of data
+    //             for (let i = 1; i <= jsonResponse.total_pages; i++) {
+    //                 fetch(movieSearch(2000, i))
+    //                     .then((res) => {
+    //                         return res.json();
+    //                     })
+    //                     .then((jsonResponse) => {
+    //                         // Push the data array of each page into arrayOfAllPages
+    //                         arrayOfAllPages.push(jsonResponse.results);
+    //                         // Test if the array have all of the pages after the loop
+    //                         if (
+    //                             arrayOfAllPages.length ===
+    //                             jsonResponse.total_pages
+    //                         ) {
+    //                             // Passing the year to create the condition and test if the release date of the movie in the range of summer period
+    //                             const selectYear = (year) => {
+    //                                 console.log(arrayOfAllPages.length);
+    //                                 const newArr = [];
+    //                                 arrayOfAllPages.map((array) => {
+    //                                     console.log(array.length);
+
+    //                                     // filter the array to get only the movies in the summer period of that year
+    //                                     array.filter((dataObj) => {
+    //                                         // const now = new Date();
+    //                                         const releasedDate = new Date(
+    //                                             dataObj.release_date
+    //                                         );
+    //                                         const newReleasedDate1 = new Date(
+    //                                             `${year}-05-01`
+    //                                         );
+    //                                         const newReleasedDate2 = new Date(
+    //                                             `${year}-09-04`
+    //                                         );
+
+    //                                         if (
+    //                                             releasedDate >=
+    //                                                 newReleasedDate1 &&
+    //                                             releasedDate <= newReleasedDate2
+    //                                         ) {
+    //                                             newArr.push(dataObj);
+    //                                             setMovies(dataObj);
+    //                                             // console.log(
+    //                                             //     `Title: ${dataObj.title}`,
+    //                                             //     `- ${
+    //                                             //         releasedDate < now
+    //                                             //             ? "Released"
+    //                                             //             : "Release"
+    //                                             //     } date: ${
+    //                                             //         dataObj.release_date
+    //                                             //     }`
+    //                                             // );
+    //                                         }
+    //                                     });
+    //                                     console.log(newArr);
+    //                                 });
+    //                             };
+    //                             selectYear(2000);
+    //                         }
+    //                     });
+    //             }
+    //         });
+    // }, []);
 
     const [list, setList] = useState([]);
+    const [movies, setMovies] = useState([]);
 
-    const handleAdd = () => {
+    useEffect(() => {
+        setMovies(results);
+    }, []);
+
+    const handleAdd = (props) => {
         const dbRef = firebase.database().ref();
-        results.map((result) => {
-            const movie = {
-                title: result.title,
-                release_date: result.release_date,
-                rate: result.vote_average,
-            };
-            dbRef.push(movie);
-        });
+        const movie = {
+            title: props.title,
+            date: props.release_date,
+        };
+        dbRef.push(movie);
     };
 
     const handleRemove = (key) => {
         const dbRef = firebase.database().ref();
         dbRef.child(key).remove();
-        // Send off a request to firebase  to remove a book with specific id
     };
 
     useEffect(() => {
@@ -168,23 +179,42 @@ function App() {
 
     return (
         <div>
-            <ul>
-                {list.map((mov) => {
-                    return (
-                        <li key={mov.key}>
-                            <p>{mov.movie.title}</p>
-                            <button
-                                onClick={() => {
-                                    handleRemove(mov.key);
-                                }}
-                            >
-                                Remove
-                            </button>
-                        </li>
-                    );
-                })}
-            </ul>
-            <button onClick={handleAdd}>Add</button>
+            <div className="search-container">
+                <h2>Searched Movies List</h2>
+                <ul className="search-list">
+                    {movies.map((result, index) => {
+                        return (
+                            <li key={index}>
+                                <p>{result.title}</p>
+                                <div>
+                                    <button onClick={() => handleAdd(result)}>
+                                        Add
+                                    </button>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            {list.length !== 0 ? (
+                <div className="prediction-container">
+                    <h2>Prediction List of Summer Movie</h2>
+                    <ul className="prediction-list">
+                        {list.map((mov) => {
+                            return (
+                                <li key={mov.key}>
+                                    <p>{mov.movie.title}</p>
+                                    <button
+                                        onClick={() => handleRemove(mov.key)}
+                                    >
+                                        Remove
+                                    </button>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            ) : null}
         </div>
     );
 }
