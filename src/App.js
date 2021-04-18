d3dc55f9543737a5bda73e39fe12b8d1e80d3063
Whitely <matthewwhitely77@gmail.com>
2021-04-18 12:02:43 -0400

    cleaned up things

--- src/App.js
+++ src/App.js
@@ -3,34 +3,6 @@ import Search from "./Search";
 import Data from "./Data";
 import { useState } from "react";
 
-function App() {
-  const [movies, setMovies] = useState([]);
-  const filterApi = (e, year, textInput) => {
-    e.preventDefault();
-    console.log(year);
-    console.log(textInput);
-    const weird = `https://api.themoviedb.org/3/discover/movie?api_key=a95c3731bb8d542ff3503355315d717a&primary_release_date.gte=${year}-05-01&primary_release_date.lte=${year}-09-04`;
-    console.log(weird);
-    fetch(weird)
-      .then((res) => {
-        return res.json();
-      })
-      .then((jsonResponse) => {
-        console.log(jsonResponse.results);
-        setMovies(jsonResponse.results);
-      });
-  };
-
-  return (
-    <div>
-      <Search filter={filterApi} />
-      <section>
-        {movies.map((e) => {
-          return <Data title={e.title} />;
-        })}
-      </section>
-    </div>
-  );
-}
+function App() {}
 
 export default App;
--- src/Search.js
+++ src/Search.js
@@ -81,4 +81,32 @@ function Search(props) {
   );
 }
 
+// const [movies, setMovies] = useState([]);
+// const filterApi = (e, year, textInput) => {
+//   e.preventDefault();
+//   console.log(year);
+//   console.log(textInput);
+//   const weird = `https://api.themoviedb.org/3/discover/movie?api_key=a95c3731bb8d542ff3503355315d717a&primary_release_date.gte=${year}-05-01&primary_release_date.lte=${year}-09-04`;
+//   console.log(weird);
+//   fetch(weird)
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((jsonResponse) => {
+//       console.log(jsonResponsenpm);
+//       setMovies(jsonResponse.results);
+//     });
+// };
+
+// return (
+//   <div>
+//     <Search filter={filterApi} />
+//     <section>
+//       {movies.map((e) => {
+//         return <Data title={e.title} />;
+//       })}
+//     </section>
+//   </div>
+// );
+// }
 export default Search;
