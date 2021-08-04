e813c861b467b14dced4621293aabd1b3d3a2455
Whitely <matthewwhitely77@gmail.com>
2021-08-04 10:17:04 -0400

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,22 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
-  
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCZ-tqGYyRT-cP9hVwOD1_pIqUkNLnLxJU",
-    authDomain: "juno-project-4.firebaseapp.com",
-    projectId: "juno-project-4",
-    storageBucket: "juno-project-4.appspot.com",
-    messagingSenderId: "31852158616",
-    appId: "1:31852158616:web:251bcf456fc70ebbf57aed"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+import firebase from "firebase/app";
+import "firebase/database";
 
-  
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBdtm0K-FLPVbzRG-D_OdCFpCdejNgAi3A",
+
+  authDomain: "project-1ed40.firebaseapp.com",
+
+  projectId: "project-1ed40",
+
+  storageBucket: "project-1ed40.appspot.com",
+
+  messagingSenderId: "23701854762",
+
+  appId: "1:23701854762:web:beee013c8846a0aecc0153",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 export default firebase;
